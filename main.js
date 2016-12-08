3833a2d20620531afc889cc4a1c1b322009a3363
burritojustice <john@mapzen.com>
2016-12-08 11:30:49 -0800

    defining default maxbounds to entire planet, this ought to be interesting

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function parseQuery (qstr) {
 var map, scene, hash, query, scene_url;
 var minz = 1;
 var maxz = 22;
-// do we need to define fitbounds here? hopefully not
+var maxbounds = [[-90,0],[90,180]]
 
 load = (function load() {
     if (detects.webgl === false) {
